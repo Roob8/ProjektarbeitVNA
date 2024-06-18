858b00a6792035f1b6a23df6bfd16f2bbe92091b
leixlax <66028786+leixlax@users.noreply.github.com>
2024-06-18 12:52:08 +0200

    Aufgeräumt
    Daten und Ordner gelöscht

--- pythonProject/src_Python/install_vnakit.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-from os.path import join
-from os import getenv
-
-assert(sys.platform == 'win32')
-VNAKIT_PACKAGE_DIR = join(getenv('ProgramFiles'), 'Vayyar', 'VNAKit', 'python', 'packages')
-subprocess.call([sys.executable, "-m", "pip", "install", "--no-index", "--find-links=%s" % VNAKIT_PACKAGE_DIR, "vnakit"])
-subprocess.call([sys.executable, "-m", "pip", "install", "--no-index", "--find-links=%s" % VNAKIT_PACKAGE_DIR, "vnakit_ex"])
